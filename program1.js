b1024aee6ad9971c443f3c3be9acfbd484324059
tejyaduvanshi <tejyaduvanshi01@gmail.com>
2024-09-30 15:34:20 +0530

    Techdome Test

--- program1.js
+++ program1.js
@@ -3,6 +3,29 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+
+      
+let stack = [];
+let bracketMap = {
+    ')': '(',
+    '}': '{',
+    ']': '['
+};
+
+for (let char of s) {
+    if (char in bracketMap) {
+        let topElement = stack.length ? stack.pop() : '#';
+        if (bracketMap[char] !== topElement) {
+            return false;
+        }
+    } else {
+        stack.push(char);
+    }
+}
+
+
+return stack.length === 0;
+
     
 };
 
